126ee18a0c441ecc3069b414ed0d37f1ee7d7048
nurulamin092 <mnurulamin092@gmail.com>
2025-08-08 07:27:22 +0600

    bug fix

--- queries/courses.js
+++ queries/courses.js
@@ -4,19 +4,21 @@ import { User } from "@/model/user-model";
 import { Testimonial } from "@/model/testimonial-model";
 import { Module } from "@/model/modules-model";
 
-export async function getCourses() {
-    const courses = await Course.find({}).populate({
-        path: "category",
-        model: Category
-    }).populate({
-        path: "instructor",
-        model: User
-    }).populate({
-        path: "modules",
-        model: Module
-    }).populate({
-        path: "testimonials",
-        model: Testimonial
-    });
+export async function getCourseList() {
+    const courses = await Course.find({})
+        .select(["title", "thumbnail", "modules", "price", "category", "instructor"])
+        .populate({
+            path: "category",
+            model: Category
+        }).populate({
+            path: "instructor",
+            model: User
+        }).populate({
+            path: "modules",
+            model: Module
+        }).populate({
+            path: "testimonials",
+            model: Testimonial
+        }).lean();
     return courses
 }
